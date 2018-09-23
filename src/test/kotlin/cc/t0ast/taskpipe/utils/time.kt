e4be32d2e7a2432965b36190851982ae6b590905
T0astBread <t0astbreaaad@gmail.com>
2018-09-23 17:47:35 +0200

    Fixed TIME_FORMATTER

--- src/test/kotlin/cc/t0ast/taskpipe/utils/time.kt
+++ src/test/kotlin/cc/t0ast/taskpipe/utils/time.kt
@@ -3,6 +3,6 @@ package cc.t0ast.taskpipe.utils
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-val TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-mm-yyyy_hh.MM") // Windows doesn't allow colons in filenames
+val TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH.mm") // Windows doesn't allow colons in filenames
 val formattedTime: String
     get() = TIME_FORMATTER.format(LocalDateTime.now())
