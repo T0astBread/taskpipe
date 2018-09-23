e245b8b653e9345990a0b82fc2320d39e9ec2dd0
T0astBread <t0astbreaaad@gmail.com>
2018-09-23 17:49:22 +0200

    Added seconds to TIME_FORMATTER

--- src/test/kotlin/cc/t0ast/taskpipe/utils/time.kt
+++ src/test/kotlin/cc/t0ast/taskpipe/utils/time.kt
@@ -3,6 +3,6 @@ package cc.t0ast.taskpipe.utils
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-val TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH.mm") // Windows doesn't allow colons in filenames
+val TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH.mm.ss") // Windows doesn't allow colons in filenames
 val formattedTime: String
     get() = TIME_FORMATTER.format(LocalDateTime.now())
