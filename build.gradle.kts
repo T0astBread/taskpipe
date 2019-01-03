b84e7d219e5f9ea5c9544a25b844d6a63316c30c
T0astBread <tb@t0ast.cc>
2019-01-03 09:21:25 +0100

    Moved code into runner subproject
    This is done in order to seperate the runner code from the CLI code that will be added in the future

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,27 +11,3 @@ version = "0.1-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
-dependencies {
-    compile(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
-    implementation("com.google.code.gson:gson:2.8.5")
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.2")
-    testCompile("org.junit.jupiter:junit-jupiter-params:5.3.2")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.2")
-    testCompile(kotlin("test"))
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
-    }
-}
-
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
