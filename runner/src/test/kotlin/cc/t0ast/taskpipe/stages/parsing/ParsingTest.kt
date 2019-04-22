030671db1f21670fe13ff8bb024a26f03256e57a
T0astBread <tb@t0ast.cc>
2019-04-22 16:23:04 +0200

    Added test for embedded module parsing

--- runner/src/test/kotlin/cc/t0ast/taskpipe/stages/parsing/ParsingTest.kt
+++ runner/src/test/kotlin/cc/t0ast/taskpipe/stages/parsing/ParsingTest.kt
@@ -1,12 +1,16 @@
 package cc.t0ast.taskpipe.stages.parsing
 
 import cc.t0ast.taskpipe.OperationMode
+import cc.t0ast.taskpipe.modules.BreakpointModule
 import cc.t0ast.taskpipe.modules.ProcessModule
 import cc.t0ast.taskpipe.test_utils.AMOUNT_OF_ENTRIES_IN_RUN
 import cc.t0ast.taskpipe.test_utils.EXAMPLE_PIPELINE_DIR
+import cc.t0ast.taskpipe.test_utils.EXAMPLE_PIPELINE_WITH_BREAKPOINT_DIR
 import org.junit.jupiter.api.Test
 import java.lang.Integer.parseInt
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class ParsingTest {
 
@@ -42,5 +46,22 @@ class ParsingTest {
         assertEquals(2, job.arguments.size)
         assertEquals("Hello world from the echo module!", job.arguments["text"])
         assertEquals("content/hellow.txt", job.arguments["target"])
+    }
+
+    @Test
+    fun testParseEmbeddedModule() {
+        val examplePipelineDir = EXAMPLE_PIPELINE_WITH_BREAKPOINT_DIR
+        val parsedPipeline = parsePipeline(examplePipelineDir)
+
+        assertEquals("ExamplePipelineWithBreakpoint", parsedPipeline.name)
+
+        assertEquals(3, parsedPipeline.jobs.size)
+
+        val breakpointJob = parsedPipeline.jobs[1]
+        assertTrue(breakpointJob.module is BreakpointModule)
+        assertEquals("This is a test breakpoint; Verify that it works correctly!", breakpointJob.arguments["message"])
+
+        checkJob0(parsedPipeline.jobs[0])
+        checkJob1(parsedPipeline.jobs[2])
     }
 }
--- runner/src/test/kotlin/cc/t0ast/taskpipe/test_utils/filesystem.kt
+++ runner/src/test/kotlin/cc/t0ast/taskpipe/test_utils/filesystem.kt
@@ -3,6 +3,7 @@ package cc.t0ast.taskpipe.test_utils
 import java.io.File
 
 val EXAMPLE_PIPELINE_DIR = File("src/test/resources/example_pipeline")
+val EXAMPLE_PIPELINE_WITH_BREAKPOINT_DIR = File("src/test/resources/example_pipeline_with_breakpoint")
 
 fun createTestDirectory(): File {
     val testDir = File("${System.getProperty("user.dir")}/test_runs/$formattedTime")
