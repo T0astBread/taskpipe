37be59c4f152bbfd7af3170923c8fe51867221d0
T0astBread <tb@t0ast.cc>
2019-04-22 15:43:56 +0200

    Added BreakpointModule

---
+++ runner/src/main/kotlin/cc/t0ast/taskpipe/modules/BreakpointModule.kt
@@ -0,0 +1,15 @@
+package cc.t0ast.taskpipe.modules
+
+import cc.t0ast.taskpipe.OperationMode
+import cc.t0ast.taskpipe.stages.running.RunContext
+import java.io.File
+
+class BreakpointModule : Module() {
+    override val name: String = "breakpoint"
+    override val supportedOperationModes: Array<OperationMode> = arrayOf(OperationMode.GROUP)
+
+    override suspend fun run(runContext: RunContext, workingDirectory: File, arguments: Map<String, Any>) {
+        val message = arguments["message"]
+        runContext.stopExecution("Breakpoint hit", message)
+    }
+}
